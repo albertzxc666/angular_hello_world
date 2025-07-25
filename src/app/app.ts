7af303af94be84e99f26ba4fe37bc5f88320d6c2
Albert <albert200504@gmail.com>
2025-07-25 17:22:36 +0300

    Task2, redesign done

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,8 +2,6 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header-component/header-component';
 import { GreetingComponent } from './greeting/greeting-component/greeting-component';
-import { ProductDetailComponent } from './productDetail/product-detail-component/product-detail-component';
-import { ProductListComponent } from './productList/product-list-component/product-list-component';
 
 @Component({
   selector: 'app-root',
